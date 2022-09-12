cecd030cb5f0082f2b2c20c0c56eea1581037bad
Nina Palma <ninapalmadev@gmail.com>
2022-09-12 16:03:25 -0300

    create a new repository and first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,24 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args){
+		Gato gato = new Gato();
+		Livros livro = new Livros();
+		
+		System.out.println(gato);
+		
+		int a = 2;
+		int v = 4;
+		
+		System.out.println("Hello World " + (a + v));
+		
+	}
+
+}
+
+class Livros {
+	String nome;
+}
